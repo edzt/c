2d937794308764576109f34fde951531ac6dbebd
edzt <stone.edzt@gmail.com>
2016-05-04 21:23:20 +0900

    Update work2.c

--- work2.c
+++ work2.c
@@ -7,7 +7,7 @@
 int main(void){
   int i,j;//forの中に書くとforを出た時に消滅してしまう！
   for(i=2; i<MAX; i++){
-    for(j=2; j<i; j++){if(!i%j) break;}
+    for(j=2; j<i; j++){if(i%j==0) break;}
     if(i==j)  printf("%d\n",j);
   }
 }
